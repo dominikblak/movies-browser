89a0b8c72d8bb0d228b9d5413a0a96bf47444715
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-15 17:25:37 +0100

    added peopleTile in MoviePage

--- src/features/MoviePage/index.js
+++ src/features/MoviePage/index.js
@@ -2,18 +2,22 @@ import { MovieSlider } from "./MovieSlider";
 import { MovieTileDetails } from "../../common/tiles/MovieTileDetails";
 import Container from "../../common/Container";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchCredits, fetchElement, resetState, selectElement, selectError, selectLoading, resetCreditsState, } from "../elementSlice";
+import { fetchCredits, fetchElement, resetState, selectElement, selectError, selectLoading, resetCreditsState, selectCast, selectCredits, } from "../elementSlice";
 import { useEffect } from "react";
 import { StateChecker } from "../../common/StateChecker";
 import { Header } from "../../common/Header";
 import { useLocation } from "react-router-dom";
+import { PeopleTilesList } from "../PeopleList/styled";
+import { PeopleTile } from "../../common/tiles/PeopleTile";
 
 export function MoviePage() {
     const dispatch = useDispatch();
     const location = useLocation();
     const pathname = location.pathname.substring(14);
     const id = pathname;
-
+    const data = useSelector(selectCredits);
+    console.log(data)
+    const cast = data.cast;
     const elementData = useSelector(selectElement);
 
     const isLoading = useSelector(selectLoading);
@@ -45,6 +49,16 @@ export function MoviePage() {
                         votes={elementData.vote_count}
                         description={elementData.overview} />
                 </Container>
+
+                <PeopleTilesList>
+                    {data.map(({name}) => {
+                        console.log(name)
+                        return (
+                        <PeopleTile
+                            title={name} />
+                            
+                    )})}
+                </PeopleTilesList>
             </StateChecker>
         </>
     )
