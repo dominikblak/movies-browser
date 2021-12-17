b2071de941d80f109f16f2b45d6a75615bee996b
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-17 21:56:16 +0100

    remove PersonLink from CreditList

--- src/features/MoviePage/CreditsList/index.js
+++ src/features/MoviePage/CreditsList/index.js
@@ -1,29 +1,29 @@
 import { Subtitle } from "../../../common/Subtitle";
 import { PeopleTile } from "../../../common/tiles/PeopleTile";
 import { PeopleTilesList } from "../../PeopleList/styled";
-import { PersonLink } from "./styled"
+import { PersonLink } from "./styled";
 
 export const CreditsList = ({ title, iterator }) => {
-    return (
+  return (
+    <>
+      {iterator ? (
         <>
-            {iterator ?
-                <>
-                    <Subtitle title={title} />
-                    <PeopleTilesList>
-                        {iterator.map(({ name, profile_path, character, job, id }) => {
-                            return (
-                                <PersonLink to={`/people/person/${id}`}>
-                                    <PeopleTile
-                                        title={name}
-                                        poster_path={profile_path}
-                                        description={character ? character : job}
-                                    />
-                                </PersonLink>
-                            )
-                        })}
-                    </PeopleTilesList>
-                </>
-                : ""}
+          <Subtitle title={title} />
+          <PeopleTilesList>
+            {iterator.map(({ name, profile_path, character, job }) => {
+              return (
+                <PeopleTile
+                  title={name}
+                  poster_path={profile_path}
+                  description={character ? character : job}
+                />
+              );
+            })}
+          </PeopleTilesList>
         </>
-    );
+      ) : (
+        ""
+      )}
+    </>
+  );
 };
